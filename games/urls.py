81e0eeecaf5946f156fa7df9f7541538d639dc3d
Bradley Ginnever <31748731+btginnever@users.noreply.github.com>
2025-02-06 17:54:08 -0600

    footer update

--- games/urls.py
+++ games/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from games.views import HomeView, MathFactsView, AnagramHuntView, GameScoresView, record_score
+from games.views import HomeView, MathFactsView, AnagramHuntView, GameScoresView, ContactUsView, record_score
 
 app_name = 'games'
 urlpatterns = [
@@ -8,5 +8,6 @@ urlpatterns = [
     path('math-facts/', MathFactsView.as_view(), name='math-facts'),
     path('anagram-hunt/', AnagramHuntView.as_view(), name='anagram-hunt'),
     path('record-score/', record_score, name="record-score"),
-    path('game-scores/', GameScoresView.as_view(), name='game-scores')
+    path('game-scores/', GameScoresView.as_view(), name='game-scores'),
+    path('contact-us/', ContactUsView.as_view(), name='contact-us'),
 ]
