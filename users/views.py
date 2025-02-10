a7cfbe6e687cbc36a1b943b387f810b1b4e53055
Bradley Ginnever <31748731+btginnever@users.noreply.github.com>
2025-02-10 10:23:22 -0600

    messages

--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+
+
+
+class MyAccountPageView(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
+    model = get_user_model()
+    form_class = CustomUserChangeForm
+    success_message = 'Update Successful'
 
 # Create your views here.
